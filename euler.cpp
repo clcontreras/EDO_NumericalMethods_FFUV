f6af5d1bc66403f804608cf0bb6acedb765369c3
Claudio Contreras <clcontreras@uv.mx>
2022-05-30 16:44:54 -0500

    Create repo

---
+++ euler.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Claudio Contreras Aburto on 5/30/22.
+//
+
+#include "euler.h"
+
+double func(){
+
+}
+
+void explicit_euler(double f,double yi,double yi1,double h){
+  /* solución paso a paso*/
+  yi1=
+}
+
+void euler_solution(double f,double y0, double t0){
+  /* Solución completa */
+  for(){explicit_euler(...);}
+};
---
+++ euler.h
@@ -0,0 +1,10 @@
+//
+// Created by Claudio Contreras Aburto on 5/30/22.
+//
+
+#ifndef EDO_NUMERICALMETHODS_FFUV_EULER_H
+#define EDO_NUMERICALMETHODS_FFUV_EULER_H
+double func();
+void euler_solution(double f,double y0, double t0)
+void explicit_euler(double f,double yi,double yi1,double h);
+#endif //EDO_NUMERICALMETHODS_FFUV_EULER_H
