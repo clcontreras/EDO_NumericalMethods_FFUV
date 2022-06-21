6c8f0fb68a60d0c6716133b33943915dc9894389
J0raa <105519448+J0raa@users.noreply.github.com>
2022-06-21 12:32:30 -0500

    Update trapecio.cpp

--- trapecio.cpp
+++ trapecio.cpp
@@ -1,44 +1,31 @@
-#include <iostream>
 #include <conio.h>
+#include <iostream> 
+#include<math.h>
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-
-using namespace std;
-
-double funcion(float x)
-{
-	return (x * x) * exp(-x * x);
+double f(double xo, double yo) { 
+    return 2 * xo + yo; 
 }
 
-int main()
-{
-	int n;
-	float i, a, b;
-	double f1, f2, f3, dx, y;
-	
-	cout << "Ingresar el limite inferior" << endl;
-	scanf_s("%f", &a);
 
-	cout << "Ingresar limite superior" << endl;
-	scanf_s("%f", &b);
-
-	cout << "Ingresar el numero de particiones" << endl;
-	scanf_s("%d", &n);
-
-	f2 = 0.0;
-	f1 = funcion(a);
-	f3 = funcion(b);
-	dx = (b - a) / (float)n;
-
-	for (i = a; i < b; i = i+dx)
-	{
-		f2 += funcion(i);
-	}
-
-	y = (f2 + (f1 + f3) / 2) * dx;
-
-	printf("%0.10f\n", y);
-
-	return 0;
+int main(){
+    double xo, xf, h, yo, ys;
+    printf("\nIngrese valor de x inicial: ");
+    scanf_s("%lf", &xo);
+    printf("\nIngrese valor de y inicial: ");
+    scanf_s("%lf", &yo);
+    printf("\nIngrese valor de x final: ");
+    scanf_s("%lf", &xf);
+    printf("\nIngrese paso: ");
+    scanf_s("%lf", &h);
+    printf("\nTabla de valores: \n\tX\t\tY");
+    while (xo <= xf){
+        ys = yo + h * f(xo, yo);
+        yo = yo + h / 2 * (f(xo, yo) + f(xo + h, ys));
+        xo = xo + h;
+        printf("\n %lf , \t %lf", xo, yo);
+    }
+    printf("\ny(k+1)= %lf",yo);
+    return 0;
 }
